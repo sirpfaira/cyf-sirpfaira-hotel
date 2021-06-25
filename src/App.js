a8121864e5bf7dc447181e76d1027dc58cb68c54
Simba Pfaira <spfaira@yahoo.com>
2021-06-26 00:36:31 +0200

    Level 2 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import Bookings from './Bookings';
+import './App.css';
+import placesToTravel from './data/placesToTravel.json';
+import Restaurant from './Restaurant';
+
+const footerInfo = [
+  '123 Fake Street, London, E1 4UD',
+  'hello@fakehotel.com',
+  '0123 456789',
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <Heading />
+      <TouristInfoCards />
+      <Bookings />
+      <Restaurant />
+      <Footer array={footerInfo} />
+    </div>
+  );
+};
+
+function TouristInfoCards() {
+  return (
+    <div className='cards-container'>
+      {placesToTravel.map((country, index) => (
+        <div key={index}>
+          <Card country={country} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Card({ country }) {
+  return (
+    <div className='card'>
+      <img src={country.img} className='card-img-top' alt={country.name} />
+      <div className='card-body'>
+        <h1 className='card-title'>{country.name}</h1>
+        <p className='card-text'>{country.desc}</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={country.link}
+          className='btn btn-primary'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          More info...
         </a>
-      </header>
+      </div>
+    </div>
+  );
+}
+
+const Heading = () => {
+  return (
+    <header className='App-header'>
+      <img
+        className='header-image'
+        src='https://image.flaticon.com/icons/svg/139/139899.svg'
+        alt='Header Logo'
+      />
+      <h3 className='header-text'>CYF Hotel</h3>
+    </header>
+  );
+};
+
+function Footer({ array }) {
+  return (
+    <div className='footer'>
+      <ul>
+        {array.map((item, index) => (
+          <li key={index}> {item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Restaurant.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Restaurant = () => {
+  return (
+    <div>
+      <h3>Restaurant Orders</h3>
+      <ul>
+        <Order orderType='Pizzas' />
+        <Order orderType='Salads' />
+        <Order orderType='Chocolate cake' />
+      </ul>
+    </div>
+  );
+};
+
+const Order = ({ orderType }) => {
+  let [orders, setOrders] = useState(0);
+
+  const orderOne = () => {
+    setOrders(orders + 1);
+  };
+
+  return (
+    <li>
+      {orderType}: {orders} <RestaurantButton action={orderOne} />
+    </li>
+  );
+};
+
+const RestaurantButton = ({ action }) => {
+  return (
+    <button className='btn btn-primary' onClick={action}>
+      Add
+    </button>
+  );
+};
+
+export default Restaurant;
