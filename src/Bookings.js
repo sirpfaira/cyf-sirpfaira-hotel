6696083796b6df5e0ebc22b18aab178bd256ed2d
Simba Pfaira <spfaira@yahoo.com>
2021-06-26 03:44:28 +0200

    Level 3 - Step 19 complete

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from './Search.js';
 import SearchResults from './SearchResults.js';
-import FakeBookings from './data/fakeBookings.json';
+//import FakeBookings from './data/fakeBookings.json';
 
 const Bookings = () => {
-  const search = (searchVal) => {
+  const [bookings, setBookings] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
+
+  const SearchFunction = (searchVal) => {
     console.info('TO DO!', searchVal);
+    setSearchWord(searchVal);
   };
-  let [bookings, setBookings] = useState(FakeBookings);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me`)
+      .then((res) => res.json())
+      .then((data) => {
+        filterData(data);
+      })
+      .catch((error) => console.error(error));
+  });
+
+  const filterData = (data) => {
+    if (searchWord) {
+      let matchingNames = data.filter(
+        (entry) =>
+          entry.firstName.toUpperCase().includes(searchWord.toUpperCase()) ||
+          entry.surname.toUpperCase().includes(searchWord.toUpperCase())
+      );
+      setBookings(matchingNames);
+    } else {
+      setBookings(data);
+    }
+  };
+
   return (
     <div className='App-content'>
       <div className='container'>
-        <Search search={search} />
+        <Search search={SearchFunction} />
         <SearchResults results={bookings} />
       </div>
     </div>
